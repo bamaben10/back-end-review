012182ef1b3a35561b4995db7a0238c26af0ac2e
Benjamin Salmon <bensalmon8@gmail.com>
2022-06-25 10:29:48 -0400

    back end duplicate review

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+const friends = ['Nitin', 'Eric', 'Jeddy', 'Cameron', 'Riley']
+
+app.get('/api/users', (req,res) => {
+    console.log(req)
+    let friends = ['Nitin', 'Eric', 'Jeddy', 'Cameron', 'Riley']
+    res.status(200).send(friends)
+})
+
+app.get('/weather/:temperature', (req,res) => {
+    const phrase = `<h3>It was ${req.params.temperature} today</h3>`
+    res.status(200).send(phrase)
+})
+
+const SERVER_PORT = 4000;
+
+app.listen(SERVER_PORT, () => {
+    console.log('Server running on port 4000.')
+})
+
+
+
